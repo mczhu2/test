1ec26db0e0173cf63d0e309cc735eb30f265061f
mczhu2 <904973404@qq.com>
2017-07-24 09:31:59 +0800

    Update Test.java class.

--- java_test/src/com/mczhu/study/main/Test.java
+++ java_test/src/com/mczhu/study/main/Test.java
@@ -2,6 +2,8 @@ package com.mczhu.study.main;
 
 public class Test {
 
+	private String name = "уехЩ";
+
 	public static void main(String args[]) {
 		System.out.println("this is a test demo !");
 	}
