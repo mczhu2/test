d5ba79588dccb51adccb00074c03e25b99411f49
mczhu2 <904973404@qq.com>
2017-07-26 11:53:25 +0800

    Update Test.java

--- java_test/src/com/mczhu/study/main/Test.java
+++ java_test/src/com/mczhu/study/main/Test.java
@@ -2,7 +2,7 @@ package com.mczhu.study.main;
 
 public class Test {
 
-	private String name = "уехЩ-";
+	private String name = "-";
 
 	public static void main(String args[]) {
 		System.out.println("this is a test demo !");
